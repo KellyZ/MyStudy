1fd2d4e186d89c0bb33b709c9f00d8be07a35f53
kelly.zhang <work2012kk@gmail.com>
2017-11-02 20:40:21 +0800

    add ios demo

---
+++ ios/demo002.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+let hello = "world"
+
+//hello = "haha"
+
+var str1: String?
+var str2: String?
+
+// 整型，浮点型，布尔型，字符串, 字符, 元祖， 复合
+// Int, Float, Bool, Double, String, Character
+var num :Int = 12
+var numF :Float = 12.0
+var isTrue :Bool = true
+var dollar :Double = 12.222
+
+var w = 13.02
+var h:Character = "h"
+
+
+var (name,age) = ("kelly",23)
+
+name = "haha"
+age = 18
+print(name,age)
+
+var gf = (name:"zhangsan",age:45)
+
+gf.name
+gf.age
+gf.0
+gf.1
+
+
+
+
